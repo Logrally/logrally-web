50610b8329056c981e8129b6d9cc6e3f4d32877c
jansorg <github@joachim-ansorg.de>
2018-10-05 22:19:12 +0200

    Use ring-ui 1.0

--- src/app/App.kt
+++ src/app/App.kt
@@ -23,40 +23,39 @@ class App : RComponent<RProps, AppState>() {
         header {
             link(title = "Homepage", href = "/")
 
-//            tray {
-//                if (state.loggedIn == true) {
-//                    button("Logout") {
-//                        attrs.onClick = {
-//                            setState { loggedIn = false }
-//                        }
-//                    }
-//                }
-//                else {
-//                    button("Login") {
-//                        attrs.onClick = {
-//                            setState { showLogin = true }
-//                        }
-//                    }
-//                }
-//            }
+            tray {
+                if (state.loggedIn == true) {
+                    button("Logout") {
+                        attrs.onClick = {
+                            setState { loggedIn = false }
+                        }
+                    }
+                } else {
+                    button("Login") {
+                        attrs.onClick = {
+                            setState { showLogin = true }
+                        }
+                    }
+                }
+            }
         }
 
-//        div {
-//            authDialog(serviceName = "ACME Inc.", show = state.showLogin) {
-//                attrs.onConfirm = {
-//                    setState { showLogin = false; loggedIn = true }
-//                }
-//            }
-//
-//            if (state.loggedIn) {
-//                h1 { +"Welcome to your application!" }
-//                loader("Loading application. Please wait ...")
-//            }
-//        }
+        div {
+            authDialog(serviceName = "ACME Inc.", show = state.showLogin) {
+                attrs.onConfirm = {
+                    setState { showLogin = false; loggedIn = true }
+                }
+            }
 
-//        footer {
-//            attrs.right = arrayOf(FooterLineData(copyright(2018), "/"), "by firstname lastname")
-//        }
+            if (state.loggedIn) {
+                h1 { +"Welcome to your application!" }
+                loader("Loading application. Please wait ...")
+            }
+        }
+
+        footer {
+            attrs.right = arrayOf(FooterLineData(copyright(2018), "/"), "by firstname lastname")
+        }
     }
 }
 
--- update-dev.bash
+++ update-dev.bash
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-rm -rf "./node-modules/@logrally/kotlin-react-ring-ui"
-(cd ../kotlin-react-ring-ui && rm -f *.tgz && gradle clean build npm_pack) && npm install ../kotlin-react-ring-ui/*.tgz
+#rm -rf "./node-modules/@logrally/kotlin-react-ring-ui"
+#(cd ../kotlin-react-ring-ui && rm -f *.tgz && gradle clean build npm_pack) && npm install ../kotlin-react-ring-ui/*.tgz
+
+cp ./docs/patched-webpack-config/* ./node_modules/react-scripts-kotlin/config/
